9a42c69074d6b981d475e68fe041cdad58616e37
fsoria <francis.soria@gmail.com>
2025-03-13 14:28:23 +0900

    Added project version retireval

--- VersionInfoApp/Program.cs
+++ VersionInfoApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "Unknown";
+
+        Console.WriteLine($"C# Project Version: {version}");
+    }
+}
